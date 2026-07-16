e19da7a0f73607d71a275819c724c1219d49fb6b
alen <alen1020@gmail.com>
2026-07-16 13:23:20 +0100

    fixed instance recognition

--- maplebot/windows.py
+++ maplebot/windows.py
@@ -88,22 +88,20 @@ except Exception:
 
 
 DEFAULT_MAPLE_FILTERS = (
-    "mapleroyals",
-    "maplelauncher",
-    "maple",
-    "mapleroyals jan",
-    "mapleroyals feb",
-    "mapleroyals mar",
-    "mapleroyals apr",
-    "mapleroyals may",
-    "mapleroyals jun",
-    "mapleroyals jul",
-    "mapleroyals aug",
-    "mapleroyals sep",
-    "mapleroyals oct",
-    "mapleroyals nov",
-    "mapleroyals dec",
+    "MapleRoyals Jan",
+    "MapleRoyals Feb",
+    "MapleRoyals Mar",
+    "MapleRoyals Apr",
+    "MapleRoyals May",
+    "MapleRoyals Jun",
+    "MapleRoyals Jul",
+    "MapleRoyals Aug",
+    "MapleRoyals Sep",
+    "MapleRoyals Oct",
+    "MapleRoyals Nov",
+    "MapleRoyals Dec",
 )
+
 
 # Window titles that match a maple filter but should still be ignored
 # (e.g. the MapleBot GUI itself is titled "MapleBot Controller").
